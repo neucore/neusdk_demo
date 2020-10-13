63c00c51593a06e3141e90a407da6954100586df
jackshengMan <peicheng.liu@neucore.com>
2020-10-13 18:52:15 +0800

    启动页添加机型选择,适配横屏6421Pose,横屏人脸关键点,横屏手势检测

---
+++ app/src/main/java/com/neucore/neusdk_demo/dialog/TransferAdminsDialog.java
@@ -0,0 +1,75 @@
+package com.neucore.neusdk_demo.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.neucore.neusdk_demo.R;
+
+
+public class TransferAdminsDialog extends Dialog {
+
+    private CheckButtonCancelOnclick buttonCancleListener;
+    private CheckButtonSureOnclick buttonSureListener;
+
+    public TransferAdminsDialog(Context context) {
+        super(context);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setCancelable(false);
+        setCanceledOnTouchOutside(false);
+        setContentView(R.layout.dialog_transfer_admins);
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        TextView dialog_disconnect_cancel = (TextView) findViewById(R.id.dialog_disconnect_cancel);
+        TextView dialog_disconnect_sure = (TextView) findViewById(R.id.dialog_disconnect_sure);
+        dialog_disconnect_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonCancleListener.onClick(v);
+            }
+        });
+
+        dialog_disconnect_sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSureListener.onClick(v);
+            }
+        });
+
+    }
+
+    //确定回调
+    public interface CheckButtonCancelOnclick {
+        void onClick(View view);
+    }
+
+    public void setButtonCancelOnClick(CheckButtonCancelOnclick buttonCancleListener) {
+        this.buttonCancleListener = buttonCancleListener;
+    }
+
+    public interface CheckButtonSureOnclick {
+        void onClick(View view);
+    }
+
+    public void setButtonSureOnClick(CheckButtonSureOnclick buttonSureListener) {
+        this.buttonSureListener = buttonSureListener;
+    }
+
+
+    public void finish() {
+        dismiss();
+    }
+
+
+
+
+
+}
