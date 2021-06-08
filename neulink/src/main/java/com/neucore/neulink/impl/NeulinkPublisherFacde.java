f7c49df3eef99b3072fecad0bd1214fccb3b6365
gongping.zhu <gongping.zhu@neucore.com>
2021-06-08 20:30:08 +0800

    删除过期接口
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/impl/NeulinkPublisherFacde.java
+++ neulink/src/main/java/com/neucore/neulink/impl/NeulinkPublisherFacde.java
@@ -8,6 +8,9 @@ import com.neucore.neulink.faceupld.FaceInfo;
 import com.neucore.neulink.faceupld.FaceUpload;
 import com.neucore.neulink.faceupld.v12.FaceRect12;
 import com.neucore.neulink.faceupld.v12.FaceUpload12;
+import com.neucore.neulink.lic.LicUpldCmd;
+import com.neucore.neulink.tmptr.FaceTemp;
+import com.neucore.neulink.tmptr.FaceTempCmd;
 import com.neucore.neulink.util.DeviceUtils;
 import com.neucore.neulink.util.JSonUtils;
 
@@ -32,81 +35,37 @@ public class NeulinkPublisherFacde {
      * 车牌上报
      * upld/req/carplateinfo/v1.0/${req_no}[/${md5}], qos=0
      */
-//    public void upldLic(String num,String color,String imageUrl,String cmpCode,String locationCode,String position){
-//        LicUpldCmd req = new LicUpldCmd();
-//        req.setNum(num);
-//        req.setColor(color);
-//        req.setImageUrl(imageUrl);
-//        req.setDeviceId(DeviceUtils.getDeviceId(context));
-//        req.setCmpCode(cmpCode);
-//        req.setLocationCode(locationCode);
-//        req.setPosition(position);
-//
-//        String payload = JSonUtils.toString(req);
-//        String topic = "upld/req/carplateinfo";
-//        service.publishMessage(topic, IProcessor.V1$0, payload, 0);
-//    }
+    public void upldLic(String num,String color,String imageUrl,String cmpCode,String locationCode,String position){
+        LicUpldCmd req = new LicUpldCmd();
+        req.setNum(num);
+        req.setColor(color);
+        req.setImageUrl(imageUrl);
+        req.setDeviceId(DeviceUtils.getDeviceId(context));
+        req.setCmpCode(cmpCode);
+        req.setLocationCode(locationCode);
+        req.setPosition(position);
+
+        String payload = JSonUtils.toString(req);
+        String topic = "upld/req/carplateinfo";
+        service.publishMessage(topic, IProcessor.V1$0, payload, 0);
+    }
 
     /**
      * 体温上报
      * upld/req/facetemprature/v1.0/${req_no}[/${md5}], qos=0
      */
-//    public void upldFacetmp(FaceTemp[] data){
-//        FaceTempCmd req = new FaceTempCmd();
-//        req.setDeviceId(DeviceUtils.getDeviceId(context));
-//        req.setData(data);
-//
-//        String payload = JSonUtils.toString(req);
-//        String topic = "upld/req/facetemprature";
-//        service.publishMessage(topic, IProcessor.V1$0, payload, 0);
-//    }
+    public void upldFacetmp(FaceTemp[] data){
+        FaceTempCmd req = new FaceTempCmd();
+        req.setDeviceId(DeviceUtils.getDeviceId(context));
+        req.setData(data);
 
-    /**
-     * 人脸检测和人脸识别数据是同时上报，区别在于人脸检测数据里包括已识别和未识别所有数据，
-     * 人脸识别数据只包含已识别的数据,recognized = 1。
-     * @param info
-     * @deprecated
-     */
-    @Deprecated
-    public void upldFaceInfo(String url,FaceInfo info){
-
-        int index = url.lastIndexOf("/");
-        String dir =url.substring(0,index);
-        String name = url.substring(index+1);
-        if(info.getDetectInfo()!=null){
-            info.getDetectInfo().setDir(dir);
-            info.getDetectInfo().setFaceImage(name);
-        }
-        info.setDeviceId(DeviceUtils.getDeviceId(context));
-        String payload = JSonUtils.toString(info);
-        String topic = "upld/req/faceinfo/"+info.getDeviceId();
+        String payload = JSonUtils.toString(req);
+        String topic = "upld/req/facetemprature";
         service.publishMessage(topic, IProcessor.V1$0, payload, 0);
     }
 
     /**
-     *
-     * @param url 人脸照片的url
-     * @param info 人脸识别信息
-     * @deprecated
-     */
-    @Deprecated
-    public void upldFaceInfo(String url,FaceUpload info){
-
-        int index = url.lastIndexOf("/");
-        String dir =url.substring(0,index);
-
-        info.setDeviceId(DeviceUtils.getDeviceId(context));
-
-        if (info.getAiData()!=null){
-            info.getAiData().setDir(dir);
-        }
-
-        String payload = JSonUtils.toString(info);
-        String topic = "upld/req/faceinfo/"+info.getDeviceId();
-        service.publishMessage(topic, IProcessor.V1$1, payload, 0);
-    }
-
-    /**
+     * 人脸上报
      * 1.2版本协议
      * @param url 人脸照片的url
      * @param info 人脸识别信息
