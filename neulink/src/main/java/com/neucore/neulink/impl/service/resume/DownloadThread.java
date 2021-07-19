8ced184a45069481b2079a9a1d93d37f51114840
gongping.zhu <gongping.zhu@neucore.com>
2021-07-19 11:54:09 +0800

    fixed 下载报错，切换至okhttp
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/impl/service/resume/DownloadThread.java
+++ neulink/src/main/java/com/neucore/neulink/impl/service/resume/DownloadThread.java
@@ -5,21 +5,25 @@ import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.util.Log;
+
+import okhttp3.Response;
 
 public class DownloadThread extends Thread {
     private static final String TAG = "DownloadThread";
     private File saveFile;
-    private URL downUrl;
-    private int block;
+    private String downUrl;
+    private long block;
     /* 下载开始位置  */
     private int threadId = -1;
     private long downLength;
     private boolean finish = false;
     private FileDownloader downloader;
 
-    public DownloadThread(FileDownloader downloader, URL downUrl, File saveFile, int block, long downLength, int threadId) {
+    public DownloadThread(FileDownloader downloader, String downUrl, File saveFile, long block, long downLength, int threadId) {
         this.downUrl = downUrl;
         this.saveFile = saveFile;
         this.block = block;
@@ -31,23 +35,25 @@ public class DownloadThread extends Thread {
 
     @Override
     public void run() {
+        Response response = null;
         if(downLength < block){//未下载完成
             try {
-                HttpURLConnection http = (HttpURLConnection) downUrl.openConnection();
-                http.setConnectTimeout(5 * 1000);
-                http.setRequestMethod("GET");
-                http.setRequestProperty("Accept", "image/gif, image/jpeg, image/pjpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*");
-                http.setRequestProperty("Accept-Language", "zh-CN");
-                http.setRequestProperty("Referer", downUrl.toString());
-                http.setRequestProperty("Charset", "UTF-8");
-                long startPos = block * (threadId - 1) + downLength;//开始位置
-                int endPos = block * threadId -1;//结束位置
-                Log.i(TAG,"线程 "+ threadId + "，开始下载的位置: " + startPos+ "，结束位置："+ endPos);
-                http.setRequestProperty("Range", "bytes=" + startPos + "-"+ endPos);//设置获取实体数据的范围
-                http.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)");
-                http.setRequestProperty("Connection", "Keep-Alive");
 
-                InputStream inStream = http.getInputStream();
+                Map<String,String> headers = new HashMap<>();
+                headers.put("Accept","image/gif, image/jpeg, image/pjpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-img, */*");
+                headers.put("Accept-Language","zh-CN");
+                headers.put("Referer",downUrl);
+                headers.put("Charset","UTF-8");
+                headers.put("User-Agent","Mozilla/4.0{Neulink} (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729;)");
+                headers.put("Connection", "Keep-Alive");
+
+                long startPos = block * (threadId - 1) + downLength;//开始位置
+                long endPos = block * threadId -1;//结束位置
+                Log.i(TAG,"线程 "+ threadId + "，开始下载的位置: " + startPos+ "，结束位置："+ endPos);
+                headers.put("Range", "bytes=" + startPos + "-"+ endPos);//设置获取实体数据的范围
+
+                response = FileDownloader.getClient(5,15).newCall(FileDownloader.createRequest(downUrl,headers)).execute();
+                InputStream inStream = response.body().byteStream();
                 byte[] buffer = new byte[2048];
                 int readed = 0;
                 print("Thread " + this.threadId + " start download from position "+ startPos);
--- neulink/src/main/java/com/neucore/neulink/impl/service/resume/FileDownloader.java
+++ neulink/src/main/java/com/neucore/neulink/impl/service/resume/FileDownloader.java
@@ -4,15 +4,23 @@ import java.io.File;
 import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.content.Context;
 import android.util.Log;
+
+import com.neucore.neulink.util.NeuHttpHelper;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * 文件下载器
@@ -34,9 +42,9 @@ public class FileDownloader {
     private Context context;
     private FileService fileService = FileService.getInstance();
     /* 已下载文件长度 */
-    private int downloadSize = 0;
+    private long downloadSize = 0;
     /* 原始文件长度 */
-    private int fileSize = 0;
+    private long fileSize = 0l;
     /* 线程数 */
     private DownloadThread[] threads;
     /* 本地保存文件 */
@@ -44,9 +52,29 @@ public class FileDownloader {
     /* 缓存各线程下载的长度*/
     private Map<Integer, Long> data = new ConcurrentHashMap<Integer, Long>();
     /* 每条线程下载的长度 */
-    private int block;
+    private long block;
     /* 下载路径  */
     private String downloadUrl;
+    static OkHttpClient getClient(int connTimeout, int readTimeout){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(connTimeout, TimeUnit.MINUTES)//设置连接超时时间
+                .readTimeout(readTimeout, TimeUnit.MINUTES)//设置读取超时时间
+                .writeTimeout(readTimeout,TimeUnit.MINUTES)
+                .build();
+        return okHttpClient;
+    }
+    static Request createRequest(String fileUrl,Map<String,String> headers){
+        //第二步构建Request对象
+        Request.Builder builder = new Request.Builder()
+                .url(fileUrl)
+                .get();
+        String[] headerKeys = new String[headers.size()];
+        headers.keySet().toArray(headerKeys);
+        for (String key:headerKeys) {
+            builder.addHeader(key,headers.get(key));
+        }
+        return builder.build();
+    }
     /**
      * 获取线程数
      */
@@ -57,7 +85,7 @@ public class FileDownloader {
      * 获取文件大小
      * @return
      */
-    public int getFileSize() {
+    public long getFileSize() {
         return fileSize;
     }
     /**
@@ -79,6 +107,7 @@ public class FileDownloader {
     protected synchronized void update(int threadId, long pos) {
         this.fileService.update(this.downloadUrl, threadId,pos);
     }
+
     /**
      * 构建文件下载器
      * @param downloadUrl 下载路径
@@ -86,30 +115,29 @@ public class FileDownloader {
      * @param threadNum 下载线程数
      */
     public FileDownloader(Context context, String downloadUrl, File fileSaveDir, int threadNum) {
+        Response response = null;
         try {
             this.context = context;
             this.downloadUrl = downloadUrl;
-            URL url = new URL(this.downloadUrl);
-            if(!fileSaveDir.exists()) fileSaveDir.mkdirs();
             this.threads = new DownloadThread[threadNum];
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setConnectTimeout(15*1000);
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "image/gif, image/jpeg, image/pjpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-img, */*");
-            conn.setRequestProperty("Accept-Language", "zh-CN");
-            conn.setRequestProperty("Referer", downloadUrl);
-            conn.setRequestProperty("Charset", "UTF-8");
-            conn.setRequestProperty("User-Agent", "Mozilla/4.0{Neulink} (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729;)");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.connect();
-            printResponseHeader(conn);
-            int code = conn.getResponseCode();
+            if(!fileSaveDir.exists()) {
+                fileSaveDir.mkdirs();
+            }
+            Map<String,String> headers = new HashMap<>();
+            headers.put("Accept","image/gif, image/jpeg, image/pjpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-img, */*");
+            headers.put("Accept-Language","zh-CN");
+            headers.put("Referer",downloadUrl);
+            headers.put("Charset","UTF-8");
+            headers.put("User-Agent","Mozilla/4.0{Neulink} (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729;)");
+            headers.put("Connection", "Keep-Alive");
+            response = getClient(5,15).newCall(createRequest(downloadUrl,headers)).execute();
+            int code = response.code();
             Log.i(TAG,code+"");
             if (code == 206||code == 200) {
-                this.fileSize = conn.getContentLength();//根据响应获取文件大小
+                this.fileSize = Long.valueOf(response.header("Content-Length"));//根据响应获取文件大小
                 if (this.fileSize <= 0) throw new RuntimeException("Unkown file size ");
 
-                String filename = getFileName(conn);//获取文件名称
+                String filename = getFileName(response);//获取文件名称
                 this.saveFile = new File(fileSaveDir, filename);//构建保存文件
                 Log.i(TAG,"开始下载到："+saveFile.getAbsolutePath());
                 /**
@@ -145,14 +173,13 @@ public class FileDownloader {
      * @return 已下载文件大小
      * @throws Exception
      */
-    public int download(DownloadProgressListener listener) throws Exception{
+    public long download(DownloadProgressListener listener) throws Exception{
         try {
             RandomAccessFile randOut = new RandomAccessFile(this.saveFile, "rw");
             if(this.fileSize>0) {
                 randOut.setLength(this.fileSize);
             }
             randOut.close();
-            URL url = new URL(this.downloadUrl);
             if(this.data.size() != this.threads.length){
                 this.data.clear();
                 for (int i = 0; i < this.threads.length; i++) {
@@ -165,7 +192,7 @@ public class FileDownloader {
                  */
                 long downLength = this.data.get(i+1);
                 if(downLength < this.block && this.downloadSize<this.fileSize){//判断线程是否已经完成下载,否则继续下载
-                    this.threads[i] = new DownloadThread(this, url, this.saveFile, this.block, this.data.get(i+1), i+1);
+                    this.threads[i] = new DownloadThread(this, downloadUrl, this.saveFile, this.block, this.data.get(i+1), i+1);
                     this.threads[i].setPriority(7);
                     this.threads[i].start();
                 }else{
@@ -183,7 +210,7 @@ public class FileDownloader {
                     if (this.threads[i] != null && !this.threads[i].isFinish()) {//如果发现线程未完成下载
                         notFinish = true;//设置标志为下载没有完成
                         if(this.threads[i].getDownLength() == -1){//如果下载失败,再重新下载
-                            this.threads[i] = new DownloadThread(this, url, this.saveFile, this.block, this.data.get(i+1), i+1);
+                            this.threads[i] = new DownloadThread(this, downloadUrl, this.saveFile, this.block, this.data.get(i+1), i+1);
                             this.threads[i].setPriority(7);
                             this.threads[i].start();
                         }
@@ -207,20 +234,13 @@ public class FileDownloader {
     /**
      * 获取文件名
      */
-    private String getFileName(HttpURLConnection conn) {
-        String filename = this.downloadUrl.substring(this.downloadUrl.lastIndexOf('/') + 1);
-        if(filename==null || "".equals(filename.trim())){//如果获取不到文件名称
-            for (int i = 0;; i++) {
-                String mine = conn.getHeaderField(i);
-                if (mine == null) break;
-                if("content-disposition".equals(conn.getHeaderFieldKey(i).toLowerCase())){
-                    Matcher m = Pattern.compile(".*filename=(.*)").matcher(mine.toLowerCase());
-                    if(m.find()) return m.group(1);
-                }
-            }
-            filename = UUID.randomUUID()+ ".tmp";//默认取一个文件名
+    private String getFileName(Response response) {
+        String filename = response.header("Content-Disposition");
+        Matcher m = Pattern.compile(".*filename=(.*)").matcher(filename);
+        if(m.find()) {
+            return m.group(1);
         }
-        return filename;
+        return UUID.randomUUID()+ ".tmp";//默认取一个文件名
     }
     /**
      * 获取Http响应头字段
