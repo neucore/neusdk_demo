241742480e802fe08b443aedabee36e95f615aae
gongping.zhu <gongping.zhu@neucore.com>
2021-09-17 09:16:31 +0800

    add 控制实现框架
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

---
+++ neulink/src/main/java/com/neucore/neulink/cmd/rrpc/RsvctrlSynCmdRes.java
@@ -0,0 +1,6 @@
+package com.neucore.neulink.cmd.rrpc;
+
+import com.neucore.neulink.impl.CmdRes;
+
+public class RsvctrlSynCmdRes extends CmdRes {
+}
--- neulink/src/main/java/com/neucore/neulink/impl/proc/RsvctrlProcessor.java
+++ neulink/src/main/java/com/neucore/neulink/impl/proc/RsvctrlProcessor.java
@@ -1,4 +1,54 @@
 package com.neucore.neulink.impl.proc;
 
-public class RsvctrlProcessor {
+import android.content.Context;
+
+import com.neucore.neulink.cmd.rrpc.RsvctrlSynCmd;
+import com.neucore.neulink.cmd.rrpc.RsvctrlSynCmdRes;
+import com.neucore.neulink.extend.ICmdListener;
+import com.neucore.neulink.impl.GProcessor;
+import com.neucore.neulink.impl.NeulinkTopicParser;
+import com.neucore.neulink.util.JSonUtils;
+
+/**
+ * 预约控制下发
+ */
+public class RsvctrlProcessor extends GProcessor<RsvctrlSynCmd, RsvctrlSynCmdRes,String> {
+    public RsvctrlProcessor(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String process(NeulinkTopicParser.Topic topic, RsvctrlSynCmd payload) {
+        return "待实现";
+    }
+
+    @Override
+    public RsvctrlSynCmd parser(String payload) {
+        return (RsvctrlSynCmd) JSonUtils.toObject(payload, RsvctrlSynCmd.class);
+    }
+
+    @Override
+    protected RsvctrlSynCmdRes responseWrapper(RsvctrlSynCmd t, String result) {
+        return null;
+    }
+
+    @Override
+    protected RsvctrlSynCmdRes fail(RsvctrlSynCmd t, String error) {
+        return null;
+    }
+
+    @Override
+    protected RsvctrlSynCmdRes fail(RsvctrlSynCmd t, int code, String error) {
+        return null;
+    }
+
+    @Override
+    protected String resTopic() {
+        return null;
+    }
+
+    @Override
+    protected ICmdListener getListener() {
+        return null;
+    }
 }
