bec0a1576ed7bc55fd387fb64cfae4e5819b0235
gongping.zhu <gongping.zhu@neucore.com>
2022-02-10 11:33:21 +0800

    调整SampleConnector onCallBack 调用次序
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

---
+++ neulink/src/main/java/com/neucore/neulink/extend/impl/HelloProcessor.java
@@ -0,0 +1,71 @@
+package com.neucore.neulink.extend.impl;
+
+import android.content.Context;
+
+import com.neucore.neulink.cmd.rrpc.AuthSyncCmd;
+import com.neucore.neulink.cmd.rrpc.AuthSyncCmdRes;
+import com.neucore.neulink.impl.GProcessor;
+import com.neucore.neulink.util.ContextHolder;
+import com.neucore.neulink.util.DeviceUtils;
+import com.neucore.neulink.util.JSonUtils;
+
+/**
+ * 设备授权下发
+ * AuthSyncCmd:请求对象，
+ * AuthSyncCmdRes：响应对象
+ * String:actionListener的返回类型
+ */
+public class HelloProcessor extends GProcessor<AuthSyncCmd, AuthSyncCmdRes,String> {
+
+    public HelloProcessor(){
+        this(ContextHolder.getInstance().getContext());
+    }
+
+    public HelloProcessor(Context context) {
+        super(context);
+    }
+
+    @Override
+    public AuthSyncCmd parser(String payload) {
+        return (AuthSyncCmd) JSonUtils.toObject(payload, AuthSyncCmd.class);
+    }
+
+    /**
+     *
+     * @param t 同步请求
+     * @param result listener.doAction 的返回值
+     * @return
+     */
+    @Override
+    protected AuthSyncCmdRes responseWrapper(AuthSyncCmd t, String result) {
+        AuthSyncCmdRes res = new AuthSyncCmdRes();
+        res.setCmdStr(t.getCmdStr());
+        res.setCode(200);
+        res.setDeviceId(DeviceUtils.getDeviceId(getContext()));
+        res.setData(result);
+        res.setMsg("成功");
+        return res;
+    }
+
+    @Override
+    protected AuthSyncCmdRes fail(AuthSyncCmd t, String error) {
+        AuthSyncCmdRes res = new AuthSyncCmdRes();
+        res.setCmdStr(t.getCmdStr());
+        res.setCode(500);
+        res.setDeviceId(DeviceUtils.getDeviceId(getContext()));
+        res.setData(error);
+        res.setMsg("失败");
+        return res;
+    }
+
+    @Override
+    protected AuthSyncCmdRes fail(AuthSyncCmd t, int code, String error) {
+        AuthSyncCmdRes res = new AuthSyncCmdRes();
+        res.setCmdStr(t.getCmdStr());
+        res.setCode(code);
+        res.setDeviceId(DeviceUtils.getDeviceId(getContext()));
+        res.setData(error);
+        res.setMsg("失败");
+        return res;
+    }
+}
