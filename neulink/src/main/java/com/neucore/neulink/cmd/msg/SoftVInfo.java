b5a4c56559c20b55ad09a0e6f62d5e19354587f4
gongping.zhu <gongping.zhu@neucore.com>
2022-02-22 11:22:00 +0800

    调整默认DeviceServiceImpl 服务实现
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/cmd/msg/SoftVInfo.java
+++ neulink/src/main/java/com/neucore/neulink/cmd/msg/SoftVInfo.java
@@ -15,6 +15,10 @@ public class SoftVInfo {
     @SerializedName("alogv")
     private String alogVersion;
 
+    /**
+     * 固件名称
+     * @return
+     */
     public String getOsName() {
         return osName;
     }
@@ -27,6 +31,10 @@ public class SoftVInfo {
         this.osName = osName;
     }
 
+    /**
+     * 固件版本
+     * @return
+     */
     public String getOsVersion() {
         return osVersion;
     }
@@ -39,26 +47,50 @@ public class SoftVInfo {
         this.osVersion = osVersion;
     }
 
+    /**
+     * 应用程序名称【apk应用名称】
+     * @return
+     */
     public String getReportName() {
         return reportName;
     }
 
+    /**
+     * 应用程序名称【apk应用名称】
+     * @param reportName
+     */
     public void setReportName(String reportName) {
         this.reportName = reportName;
     }
 
+    /**
+     * 应用程序版本【apk应用程序版本】
+     * @return
+     */
     public String getReportVersion() {
         return reportVersion;
     }
 
+    /**
+     * 应用程序版本【apk应用程序版本】
+     * @param reportVersion
+     */
     public void setReportVersion(String reportVersion) {
         this.reportVersion = reportVersion;
     }
 
+    /**
+     * 算法版本
+     * @return
+     */
     public String getAlogVersion() {
         return alogVersion;
     }
 
+    /**
+     * 算法版本
+     * @param alogVersion
+     */
     public void setAlogVersion(String alogVersion) {
         this.alogVersion = alogVersion;
     }
--- neulink/src/main/java/com/neucore/neulink/impl/service/device/DeviceServiceImpl.java
+++ neulink/src/main/java/com/neucore/neulink/impl/service/device/DeviceServiceImpl.java
@@ -1,8 +1,17 @@
 package com.neucore.neulink.impl.service.device;
 
+import android.content.Context;
+import android.os.Build;
+
 import com.neucore.neulink.cmd.msg.DeviceInfo;
+import com.neucore.neulink.cmd.msg.MiscInfo;
+import com.neucore.neulink.cmd.msg.SoftVInfo;
+import com.neucore.neulink.util.AppUtils;
 import com.neucore.neulink.util.ContextHolder;
 import com.neucore.neulink.util.DeviceUtils;
+import com.neucore.neulink.util.MacHelper;
+
+import cn.hutool.core.util.ObjectUtil;
 
 public class DeviceServiceImpl implements IDeviceService {
 
@@ -16,8 +25,55 @@ public class DeviceServiceImpl implements IDeviceService {
         return DeviceUtils.getCPUSN(ContextHolder.getInstance().getContext());
     }
 
+
+
     @Override
     public DeviceInfo getInfo() {
-        return null;
+
+        DeviceInfo deviceInfo = new DeviceInfo();
+        /**
+         * cpu_sn@@ext_sn@@device_type
+         */
+        Context context = ContextHolder.getInstance().getContext();
+
+        String mac = MacHelper.getEthernetMac();
+        if(ObjectUtil.isEmpty(mac)){
+            mac = MacHelper.getWifiMac(context);
+        }
+
+        deviceInfo.setMac(mac);
+
+        deviceInfo.setTag(AppUtils.getVersionName(context));
+        MiscInfo miscInfo = new MiscInfo();
+        miscInfo.setLocalIp(DeviceUtils.getIpAddress(context));
+        miscInfo.setDescription("Jeff@amlogic");
+
+        deviceInfo.setMiscInfo(miscInfo);
+
+        SoftVInfo vInfo = new SoftVInfo();
+
+        vInfo.setOsName(android.os.Build.MANUFACTURER+"@"+android.os.Build.PRODUCT);
+        /**
+         * 固件版本
+         */
+        vInfo.setOsVersion(DeviceUtils.getSystemProperties("ro.product.releaseversion","V0.0.0"));
+        /**
+         * apk名称
+         */
+        vInfo.setReportName(AppUtils.getApkName(context));
+        /**
+         * apk版本信息
+         */
+        vInfo.setReportVersion(AppUtils.getVersionName(context));
+        vInfo.setAlogVersion("1.0");
+        deviceInfo.setSoftVInfo(vInfo);
+
+        deviceInfo.setCpuMode(Build.MODEL);
+        deviceInfo.setNpuMode(DeviceUtils.getNpuMode(context));
+
+        String[] funList = {"face"};//人脸识别
+        deviceInfo.setFunList(funList);
+        deviceInfo.setSkuToken(DeviceUtils.getSkuToken());
+        return deviceInfo;
     }
 }
