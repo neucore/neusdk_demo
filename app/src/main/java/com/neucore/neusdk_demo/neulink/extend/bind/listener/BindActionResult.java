830f76935dca7452e28bcba12d607a0a87a0b7b7
gongping.zhu <gongping.zhu@neucore.com>
2022-03-18 16:08:35 +0800

    绑定收到消息直接返回200
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- app/src/main/java/com/neucore/neusdk_demo/neulink/extend/bind/listener/BindActionResult.java
+++ app/src/main/java/com/neucore/neusdk_demo/neulink/extend/bind/listener/BindActionResult.java
@@ -10,19 +10,4 @@ import com.neucore.neulink.util.IActionResult;
  */
 public class BindActionResult extends ActionResult {
 
-    @SerializedName("mode")   //bind：绑定，unbind：解绑
-    String mode;
-
-    @SerializedName("user")
-    String user;
-
-    @SerializedName("role")   //0:普通用户；1:管理员；2:超级管理员
-    int role;
-
-
-    public void toLogcat()
-    {
-
-    }
-
 }
--- app/src/main/java/com/neucore/neusdk_demo/neulink/extend/bind/listener/BindCmdListener.java
+++ app/src/main/java/com/neucore/neusdk_demo/neulink/extend/bind/listener/BindCmdListener.java
@@ -1,11 +1,8 @@
 package com.neucore.neusdk_demo.neulink.extend.bind.listener;
 
-import android.util.Log;
-
 import com.neucore.neulink.ICmdListener;
 import com.neucore.neulink.app.NeulinkConst;
 import com.neucore.neulink.extend.NeulinkEvent;
-
 import com.neucore.neusdk_demo.neulink.extend.bind.request.BindSyncCmd;
 /**
  * 云端下发至设备端的命令侦听器
@@ -16,25 +13,12 @@ public class BindCmdListener implements ICmdListener<BindActionResult, BindSyncC
     @Override
     public BindActionResult doAction(NeulinkEvent<BindSyncCmd> neulinkEvent) {
         BindSyncCmd cmd = neulinkEvent.getSource();
-
-        //绑定事件
-        toUserUpload(true);
         /**
-         * @TODO: 业务处理；eg：保存
+         * @TODO: 业务处理；异步弹框显示请求
          */
         BindActionResult result = new BindActionResult();
-        result.setCode(NeulinkConst.STATUS_202);//需要审核确认
+        result.setCode(NeulinkConst.STATUS_200);
         result.setMessage(NeulinkConst.MESSAGE_SUCCESS);
-        result.toLogcat();
         return result;
     }
-
-
-    //--------------------------------------------------------------------
-    public void toUserUpload(boolean flag)
-    {
-        Log.i(TAG,"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@hello@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-    }
-    //--------------------------------------------------------------------
-
 }
