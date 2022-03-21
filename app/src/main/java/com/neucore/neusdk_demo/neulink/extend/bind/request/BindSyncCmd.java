86390b72204a9e42002030fd7903969ca6ad0ddd
gongping.zhu <gongping.zhu@neucore.com>
2022-03-21 15:28:17 +0800

    binding 新增用户名
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- app/src/main/java/com/neucore/neusdk_demo/neulink/extend/bind/request/BindSyncCmd.java
+++ app/src/main/java/com/neucore/neusdk_demo/neulink/extend/bind/request/BindSyncCmd.java
@@ -1,17 +1,32 @@
 package com.neucore.neusdk_demo.neulink.extend.bind.request;
 
+import com.google.gson.annotations.SerializedName;
 import com.neucore.neulink.impl.Cmd;
 
 public class BindSyncCmd extends Cmd {
-    private String user;
+
+    @SerializedName("user")
+    private String userId;
+
+    @SerializedName("name")
+    private String userName;
+
     private Integer role;
 
-    public String getUser() {
-        return user;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    public void setUserId(String user) {
+        this.userId = user;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public Integer getRole() {
