51c2daaf07654e836754d25c598337ea8e8486e5
gongping.zhu <gongping.zhu@neucore.com>
2022-05-19 03:37:03 +0800

    neulink cmd & cmdRes新增Heads
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/impl/GBatchProcessor.java
+++ neulink/src/main/java/com/neucore/neulink/impl/GBatchProcessor.java
@@ -43,7 +43,7 @@ public abstract class GBatchProcessor<Req extends PkgCmd, Res extends PkgRes, Ac
          * 发送响应消息给到服务端
          */
         String topicPrefix = String.format("%s/%s/%s",topic.getPrefix(),"res",topic.getBiz());
-        NeulinkService.getInstance().getPublisherFacde().response(topicPrefix,topic.getBiz(),topic.getVersion(),topic.getReqId(),NEULINK_MODE_RECEIVE,STATUS_201, NeulinkConst.MESSAGE_PROCESSING,null);
+
         //检查当前请求是否已经已经到达过
         synchronized (lock){
             msg = query(topic.getReqId());
@@ -74,6 +74,10 @@ public abstract class GBatchProcessor<Req extends PkgCmd, Res extends PkgRes, Ac
                 req.setReqId(topic.getReqId());
                 req.setReqtime(reqTime);
                 req.setVersion(topic.getVersion());
+                /**
+                 * 响应消息已到达
+                 */
+                NeulinkService.getInstance().getPublisherFacde().response(topicPrefix,topic.getBiz(),topic.getVersion(),topic.getReqId(),NEULINK_MODE_RECEIVE,STATUS_201, NeulinkConst.MESSAGE_PROCESSING,req.getHeaders(),null);
 
                 long pages = req.getPages();
 
