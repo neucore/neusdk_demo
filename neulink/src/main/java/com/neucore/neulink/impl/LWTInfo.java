6062ecad007d01f16a2d3ce61986e78b2a73c31a
gongping.zhu <gongping.zhu@neucore.com>
2022-05-28 20:41:37 +0800

    LWTInfo 新增 qos、retained 参数
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/impl/LWTInfo.java
+++ neulink/src/main/java/com/neucore/neulink/impl/LWTInfo.java
@@ -4,6 +4,24 @@ public class LWTInfo {
 
     private String topicPrefix;
     private String payload;
+    private int qos = 1;
+    private boolean retained=false;
+
+    public int getQos() {
+        return qos;
+    }
+
+    public void setQos(int qos) {
+        this.qos = qos;
+    }
+
+    public boolean getRetained() {
+        return retained;
+    }
+
+    public void setRetained(boolean retained) {
+        this.retained = retained;
+    }
 
     public String getTopicPrefix() {
         return topicPrefix;
