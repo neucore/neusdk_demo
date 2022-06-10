a68a1befe6632ed6f38349566b9000b870782950
gongping.zhu <gongping.zhu@neucore.com>
2022-06-10 17:29:02 +0800

    重构app
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

---
+++ app/src/main/java/com/neucore/neusdk_demo/neulink/extend/device/MyDeviceServiceImpl.java
@@ -0,0 +1,37 @@
+package com.neucore.neusdk_demo.neulink.extend.device;
+
+import com.neucore.neulink.impl.cmd.msg.DeviceInfo;
+import com.neucore.neulink.impl.service.device.DefaultDeviceServiceImpl;
+import com.neucore.neulink.impl.service.device.DeviceInfoDefaultBuilder;
+import com.neucore.neulink.util.DeviceUtils;
+
+import java.util.Locale;
+
+public class MyDeviceServiceImpl extends DefaultDeviceServiceImpl {
+    @Override
+    public String getExtSN() {
+        /**
+         * 需要获取设备唯一标识【自定义，eg：YekerID@MacAddress】
+         */
+        return DimSystemVer.getInstance().getYekerId()+"@"+ DeviceUtils.getMacAddress();
+    }
+    @Override
+    public Locale getLocale(){
+        /**
+         * 需要读取apk国际化设置后的值
+         */
+        return Locale.getDefault();
+    }
+    @Override
+    public DeviceInfo getInfo() {
+        /**
+         * 需要上报应用列表【名称及其相关版本；】
+         * OTA升级文件规则
+         *
+         * ota_[sys|apk|app]_设备硬件型号_设备产品型号(对应neulink的cpu型号)_产品当前版本识别号，其中设备硬件型号和设备产品型号，以及产品当前版本识别号不能有下划线。
+         *
+         * ota升级文件包的【设备产品型号】字段需要和neulink内的 -- cpumd 进行一致；
+         */
+        return DeviceInfoDefaultBuilder.getInstance().build(new MyExtendInfoCallBack());
+    }
+}
