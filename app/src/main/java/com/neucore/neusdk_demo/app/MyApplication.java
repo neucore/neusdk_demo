fc999c20602d27630a3a12e614d6f505a9b84269
gongping.zhu <gongping.zhu@neucore.com>
2022-06-14 21:25:13 +0800

    调整
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- app/src/main/java/com/neucore/neusdk_demo/app/MyApplication.java
+++ app/src/main/java/com/neucore/neusdk_demo/app/MyApplication.java
@@ -1,48 +1,9 @@
 package com.neucore.neusdk_demo.app;
 
-import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
-import android.util.Log;
 
-import com.neucore.neulink.IDeviceExtendInfoCallback;
-import com.neucore.neulink.IDeviceService;
-import com.neucore.neulink.IExtendCallback;
-import com.neucore.neulink.ILoginCallback;
-import com.neucore.neulink.IMqttCallBack;
-import com.neucore.neulink.NeulinkConst;
-import com.neucore.neulink.impl.NeulinkService;
-import com.neucore.neulink.impl.ResCallback2Log;
-import com.neucore.neulink.impl.SampleConnector;
-import com.neucore.neulink.impl.cmd.cfg.ConfigContext;
-import com.neucore.neulink.impl.cmd.msg.DeviceInfo;
-import com.neucore.neulink.impl.cmd.msg.SoftVInfo;
-import com.neucore.neulink.impl.cmd.msg.SubApp;
-import com.neucore.neulink.impl.registry.ProcessRegistry;
-import com.neucore.neulink.impl.service.device.DefaultDeviceServiceImpl;
-import com.neucore.neulink.impl.service.device.DeviceInfoDefaultBuilder;
-import com.neucore.neulink.util.ContextHolder;
-import com.neucore.neulink.util.DeviceUtils;
 import com.neucore.neusdk_demo.neulink.MyInstaller;
-import com.neucore.neusdk_demo.neulink.extend.auth.AuthProcessor;
-import com.neucore.neusdk_demo.neulink.extend.auth.listener.AuthCmdListener;
-import com.neucore.neusdk_demo.neulink.extend.bind.BindProcessor;
-import com.neucore.neusdk_demo.neulink.extend.bind.listener.BindCmdListener;
-import com.neucore.neusdk_demo.neulink.extend.device.DimSystemVer;
-import com.neucore.neusdk_demo.neulink.extend.device.MyDeviceServiceImpl;
-import com.neucore.neusdk_demo.neulink.extend.device.MyExtendInfoCallBack;
-import com.neucore.neusdk_demo.neulink.extend.hello.HelloProcessor;
-import com.neucore.neusdk_demo.neulink.extend.hello.listener.HelloCmdListener;
-import com.neucore.neusdk_demo.neulink.extend.hello.response.HellResCallback;
-import com.neucore.neusdk_demo.service.impl.UserService;
-
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
 
 public class MyApplication extends Application
 {
