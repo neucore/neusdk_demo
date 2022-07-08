5c29686cd723352888a76e2921451ec6c51e7792
gongping.zhu <gongping.zhu@neucore.com>
2022-07-08 13:48:52 +0800

    新增ReqId
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

---
+++ neulink/src/main/java/com/neucore/neulink/log/MyPatternParser.java
@@ -0,0 +1,34 @@
+package com.neucore.neulink.log;
+
+import com.neucore.neulink.util.RequestContext;
+
+import org.apache.log4j.helpers.FormattingInfo;
+import org.apache.log4j.helpers.PatternConverter;
+import org.apache.log4j.helpers.PatternParser;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class MyPatternParser extends PatternParser {
+    int counter = 0;
+    public MyPatternParser(String pattern) {
+        super(pattern);
+    }
+
+    public void finalizeConverter(char c) {
+        if (c == 'I') {
+            addConverter(new UserDirPatternConverter(formattingInfo));
+            currentLiteral.setLength(0);
+        } else {
+            super.finalizeConverter(c);
+        }
+    }
+
+    private class UserDirPatternConverter extends PatternConverter {
+        UserDirPatternConverter(FormattingInfo formattingInfo) {
+            super(formattingInfo);
+        }
+
+        public String convert(LoggingEvent event) {
+            return RequestContext.getId();
+        }
+    }
+}
