e833e0cd0c77e3e61d6137faa4718aedbb2a04bf
gongping.zhu <gongping.zhu@neucore.com>
2022-10-24 20:27:22 +0800

    重构HttpResumeDownloader 实现
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/impl/down/http/HttpResumeDownloader.java
+++ neulink/src/main/java/com/neucore/neulink/impl/down/http/HttpResumeDownloader.java
@@ -9,21 +9,6 @@ import com.neucore.neulink.NeulinkConst;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * 文件下载器
- * OTAFileResumeDownloader loader = new OTAFileResumeDownloader(context, "http://browse.babasport.com/ejb3/ActivePort.exe",
- new File("D:\\androidsoft\\test"), 2);
- loader.getFileSize();//得到文件总大小
- try {
- loader.download(new DownloadProgressListener(){
- public void onDownloadSize(int size) {
- print("已经下载："+ size);
- }
- });
- } catch (Exception e) {
- e.printStackTrace();
- }
- */
 public class HttpResumeDownloader implements IResumeDownloader, NeulinkConst{
     /**
      * 构建文件下载器
@@ -38,7 +23,7 @@ public class HttpResumeDownloader implements IResumeDownloader, NeulinkConst{
      */
     @Override
     public File start(Context context, String reqNo,String url, IDownloadProgressListener listener) throws Exception {
-        HttpResumableDownloadRequest resumableDownloadRequest = new HttpResumableDownloadRequest();
-        return resumableDownloadRequest.start(context,reqNo,url,listener);
+        ResumeDownloadRequest resumeDownloadRequest = new ResumeDownloadRequest();
+        return resumeDownloadRequest.start(context,reqNo,url,listener);
     }
 }
