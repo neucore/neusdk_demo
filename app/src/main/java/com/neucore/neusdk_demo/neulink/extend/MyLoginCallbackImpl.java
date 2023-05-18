651e39e63230ed7a3ffac8da3b998b1d37a6f844
jackshengMan <16773961+jackshengMan@users.noreply.github.com>
2023-05-18 16:00:05 +0800

    6443算法，变更为算法内部比较，节省上层应用时间，更快更准

--- app/src/main/java/com/neucore/neusdk_demo/neulink/extend/MyLoginCallbackImpl.java
+++ app/src/main/java/com/neucore/neusdk_demo/neulink/extend/MyLoginCallbackImpl.java
@@ -1,6 +1,8 @@
 package com.neucore.neusdk_demo.neulink.extend;
 
 import com.neucore.neulink.ILoginCallback;
+import com.neucore.neulink.NeulinkConst;
+import com.neucore.neulink.impl.cmd.cfg.ConfigContext;
 import com.neucore.neulink.log.NeuLogUtils;
 import com.neucore.neulink.util.NeuHttpHelper;
 
@@ -12,15 +14,10 @@ import cn.hutool.json.JSONObject;
 /**
  * 登录服务回调实现
  */
-public class MyLoginCallbackImpl implements ILoginCallback {
+public class MyLoginCallbackImpl implements ILoginCallback, NeulinkConst {
     private String TAG = "MyLoginCallbackImpl";
     @Override
     public String login() {
-        /**
-         * 实现登录返回token
-         */
-        //NeuLogUtils.iTag(TAG,"login...");
-        //return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicmVzMSJdLCJzY29wZUlkIjoyLCJyb2xlIjoxLCJ1c2VyX2lkIjoyMSwidXNlcl9uYW1lIjoie1wiaWRcIjoyMSxcInNjb3BlSWRcIjoyLFwidXNlcm5hbWVcIjpcIjEyOTk1ODY3ODhAcXEuY29tXCIsXCJwYXNzd29yZFwiOm51bGwsXCJmdWxsbmFtZVwiOlwi5bCR5LyfMVwiLFwiZW1haWxcIjpcIjEyOTk1ODY3ODhAcXEuY29tXCIsXCJwaG9uZU51bWJlclwiOm51bGwsXCJleHRlcm5hbElkXCI6bnVsbCxcImhlYWRQb3J0cmFpdFwiOlwiaHR0cHM6Ly9uZXVkZXZpY2Uub3NzLWNuLXNoYW5naGFpLmFsaXl1bmNzLmNvbS9nZnJhbWUvMi91c2Vycy8yMS9ocC9hdmF0YXIucG5nXCIsXCJ0eXBlXCI6NCxcInJvbGVcIjoxLFwiZGVzY3JpcHRpb25cIjpudWxsLFwiaXBcIjpudWxsLFwib3B0bG9ja1wiOjEsXCJleHBpcmF0aW9uRGF0ZVwiOjE2NTE4ODU2MzQwMDAsXCJzdGF0dXNcIjowLFwiaXNEZWxcIjowLFwiY3JlYXRlZE9uXCI6MTY1MTg4NTYzNDAwMCxcIm1vZGlmaWVkT25cIjoxNjYxMTU2NDM3MDAwfSIsInNjb3BlIjpbIlJPTEVfQURNSU4iLCJST0xFX1VTRVIiLCJST0xFX0FQSSJdLCJleHAiOjE2NjE0NzE2MjYsInR5cGUiOjQsImF1dGhvcml0aWVzIjpbImFsbCJdLCJqdGkiOiI2M2JlZTk1Ni1jNWQ1LTQ0ZGMtODU4Mi1jMTMxMTJiMTNlZDMiLCJjbGllbnRfaWQiOiJnZW1pbmkifQ.hdfzRxMKM9k8jsZ_qHvkpcq-__VJ9hIOXUujXZLKWFM";
         /**
          * 实现登录返回token
          */
@@ -29,18 +26,23 @@ public class MyLoginCallbackImpl implements ILoginCallback {
         Map<String,String> headers = new HashMap<>();
         headers.put("accept-language","zh-Hans-CN");
         headers.put("mate-scope","Mg");
-        headers.put("from","2");
+        headers.put("from","3");
+        /**
+         * 新增时区字段
+         */
+        headers.put("ltz", ConfigContext.getInstance().getConfig(TimeZoneId,TimeZoneId_Asia$ShangHai));
 
         Map<String,String> params = new HashMap<>();
-        params.put("client_id","client-smrtlib");
-        params.put("client_secret","client-smrtlib-secret");
-        params.put("grant_type","password");
-        params.put("username","15800860806");
-        params.put("password","123456");
+        params.put("client_id","gemini");//client-smrtlib,gemini
+        params.put("client_secret","secret");//client-smrtlib-secret,secret
+        params.put("grant_type","password");//password
+        params.put("username","frame2");//15800860806,frame2
+        params.put("password","123456");//123456
 
+        String url = "http://10.18.9.35:18091/oauth/token";
+//        String url = "https://dev.magm.com.cn/api/uaa/oauth/token";
 
-        String response = NeuHttpHelper.post("http://47.118.59.46:18093/v1/oauth/token",params,headers,3);
-//        String response = NeuHttpHelper.post(Util.getRetrofitBaseUrl() + "/v1/oauth/token",params,headers,3);
+        String response = NeuHttpHelper.post(url,params,headers,3);
 
         JSONObject jsonObject = new JSONObject(response);
         String accessToken = ((JSONObject)jsonObject.get("data")).getStr("access_token");
