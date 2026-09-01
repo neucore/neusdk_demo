243c7fcbb5ea1fd956865b2bb197415fdc47d99e
gongping.zhu <gongping.zhu@neucore.com>
2026-09-01 16:41:40 +0800

    fixed PropChgWatcher 空指针bug
    Signed-off-by: gongping.zhu <gongping.zhu@neucore.com>

--- neulink/src/main/java/com/neucore/neulink/impl/PropChgWatcher.java
+++ neulink/src/main/java/com/neucore/neulink/impl/PropChgWatcher.java
@@ -25,39 +25,41 @@ public class PropChgWatcher extends Thread implements NeulinkConst {
             }
             catch (Exception e){}
             Properties props = SystemProperties.get();
-            int size = props.size();
-            String[] keys = new String[size];
-            props.keySet().toArray(keys);
-            List<SysPropAction> adds = new ArrayList<>();
-            List<SysPropAction> updates = new ArrayList<>();
-            List<SysPropAction> deletes = new ArrayList<>();
-            for (String key:keys){
-                if(!last.containsKey(key)){
-                    adds.add(new SysPropAction(PROP_CHG_ACTION_ADD,key,props.getProperty(key)));
-                }
-                else if(last.containsKey(key)){
-                    if(ObjectUtil.isNotEmpty(last.get(key))
-                            && ObjectUtil.isNotEmpty(props.getProperty(key))
-                            && !last.getProperty(key).equalsIgnoreCase(props.getProperty(key))){
-                        updates.add(new SysPropAction(PROP_CHG_ACTION_UPD,key,props.getProperty(key)));
-                    }
-                    else if(ObjectUtil.isNotEmpty(last.get(key))
-                            && ObjectUtil.isEmpty(props.get(key))){
-                        deletes.add(new SysPropAction(PROP_CHG_ACTION_DEL,key,props.getProperty(key)));
-                    }
-                    else if(ObjectUtil.isEmpty(last.get(key))
-                            && ObjectUtil.isNotEmpty(props.get(key))){
+            if(ObjectUtil.isNotEmpty(props)){
+                int size = props.size();
+                String[] keys = new String[size];
+                props.keySet().toArray(keys);
+                List<SysPropAction> adds = new ArrayList<>();
+                List<SysPropAction> updates = new ArrayList<>();
+                List<SysPropAction> deletes = new ArrayList<>();
+                for (String key:keys){
+                    if(!last.containsKey(key)){
                         adds.add(new SysPropAction(PROP_CHG_ACTION_ADD,key,props.getProperty(key)));
                     }
+                    else if(last.containsKey(key)){
+                        if(ObjectUtil.isNotEmpty(last.get(key))
+                                && ObjectUtil.isNotEmpty(props.getProperty(key))
+                                && !last.getProperty(key).equalsIgnoreCase(props.getProperty(key))){
+                            updates.add(new SysPropAction(PROP_CHG_ACTION_UPD,key,props.getProperty(key)));
+                        }
+                        else if(ObjectUtil.isNotEmpty(last.get(key))
+                                && ObjectUtil.isEmpty(props.get(key))){
+                            deletes.add(new SysPropAction(PROP_CHG_ACTION_DEL,key,props.getProperty(key)));
+                        }
+                        else if(ObjectUtil.isEmpty(last.get(key))
+                                && ObjectUtil.isNotEmpty(props.get(key))){
+                            adds.add(new SysPropAction(PROP_CHG_ACTION_ADD,key,props.getProperty(key)));
+                        }
+                    }
                 }
-            }
-            last = props;
-            List<SysPropAction> chgs = new ArrayList<>();
-            chgs.addAll(adds);
-            chgs.addAll(updates);
-            chgs.addAll(deletes);
-            if(ObjectUtil.isNotEmpty(chgs)){
-                ListenerRegistry.getInstance().fireProChgListener(chgs);
+                last = props;
+                List<SysPropAction> chgs = new ArrayList<>();
+                chgs.addAll(adds);
+                chgs.addAll(updates);
+                chgs.addAll(deletes);
+                if(ObjectUtil.isNotEmpty(chgs)){
+                    ListenerRegistry.getInstance().fireProChgListener(chgs);
+                }
             }
         }
     }
